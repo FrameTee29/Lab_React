6d62145aaa74da4e458548d84b2058c3b97fc5e6
FrameTee29 <teeraphat123@hotmail.com>
2019-09-09 04:49:41 +0700

    prepareStateFromWord

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react'
 import CharacterCard from './Character'
 import './App.css'
+import _ from 'lodash';
+
+
+
 const word = "Hello";
 
+const prepareStateFromWord = (given_word) => {
+  let word = given_word.toUpperCase()
+  let chars = _.shuffle(Array.from(word))
+  return {
+  word,
+  chars,
+  attempt: 1,
+  guess: [],
+  completed: false
+  }
+ }
+ 
+
 class App extends React.Component {
+
+  state= prepareStateFromWord(word); 
 
   activationHandler= (value) =>{
     console.log(value);
@@ -12,7 +31,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        {Array.from(word).map((item, index) => <CharacterCard value={item} key={index}  activationHandler= {this. activationHandler}/>)}
+        {Array.from(this.state.chars).map((item, index) => <CharacterCard value={item} key={index}  activationHandler= {this. activationHandler}/>)}
         
       </div>
     );
